14cef256e7aa1dbede516086a66387ce802a0b0a
Muhammed Asif <140389446+asifxohd@users.noreply.github.com>
2024-07-12 13:46:01 +0530

    requirements.txt file added

--- VMS/settings.py
+++ VMS/settings.py
@@ -96,13 +96,13 @@ WSGI_APPLICATION = 'VMS.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'vms',
-        'USER': 'postgres',
-        'PASSWORD': 'Asif@123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mydatabase",
+        "USER": "mydatabaseuser",
+        "PASSWORD": "mypassword",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
